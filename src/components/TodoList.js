9fb1520c3cd42047064c8f9b7b1e7ef61f9b6f92
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-06-07 16:23:44 -0400

    Creating ToDoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import Item from "./Item";
+
+const ToDoList = props => {
+    return (
+        <div className="todo-list">
+            {props.toDoItems.map(item => (
+                <Item 
+                key={item.id} 
+                item={item}
+                toggleItem={props.toggleItem}
+                />
+            ))}
+            <button className='clear-button' onClick={props.clearItems}>
+            Clear Items
+            </button>
+        </div>
+    )
+};
+
+export default ToDoList;
